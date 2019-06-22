d65a433be74591100d825d48bbb3c34f49b6e40f
Simon <semenplyaskin@gmail.com>
2019-06-22 17:15:48 +0200

    lessonJavaScript2

---
+++ lessonJavaScript2.2/js.js
@@ -0,0 +1,41 @@
+function first (){
+    // что то делаем
+    setTimeout(function(){
+        console.log(1);
+    }, 500 );
+}
+
+function second (){
+    console.log(2);
+}
+
+first();
+second();
+
+
+
+// Callback( I ) 
+
+function learnJS (lang, callback){
+    console.log("Я учу " + lang);
+    callback();
+}
+
+learnJS("Javascript", function(){
+    console.log("Я прошел 3й урок")
+});
+
+// Callback( II ) 
+
+
+function learnJS (lang, callback){
+    console.log("Я учу " + lang);
+    callback();
+}
+
+function done(){
+    console.log("Я прошел 3й урок")
+
+}
+
+learnJS("Javascript", done);
